35ae84500694f3dbb81749420da67982eefafda6
Francagir <francagir@hotmail.com>
2022-12-19 08:08:47 +0100

    Fix UI

--- Assets/_project/Scripts/Bomb.cs
+++ Assets/_project/Scripts/Bomb.cs
@@ -1,29 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Bomb : MonoBehaviour
 {
-     [SerializeField] private string bombColor;   
+     [SerializeField] private string bombColor;
+    [SerializeField] private TextMeshProUGUI finalTXT;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.rigidbody.gameObject.CompareTag("Player")) {
 
             if (GameManager.instance.CheckColor(bombColor)) {
-                gameObject.SetActive(false);
-                GameManager.instance.bombsDeactivated.Add(this.gameObject);
-                if (bombColor.Equals("amarillo")) { }//WIN
+                this.gameObject.SetActive(false);
+                GameManager.instance.bombsDeactivated.Add(this.gameObject);//add this bomb
+                if (bombColor.Equals("amarillo")) { //last color
+                    GameManager.instance.deathMenu.SetActive(true);//activamos menu muerte
+                    finalTXT.text = "You deactivated the bombs";//Cambiamos TXT
+                }//WIN
             } 
             else if(!GameManager.instance.CheckColor(bombColor)) {
                 foreach (GameObject bomba in GameManager.instance.bombsDeactivated) {
-                    bomba.SetActive(true);                   
+                    bomba.SetActive(true); //Reactivamos bombas
                 }
             }       
         }
     }
-       
-
 }
 
